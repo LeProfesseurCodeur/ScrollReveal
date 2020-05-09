862dca44527c08da74317afab00c1eb14ff57bee
Dylan LUCHMUN <dylan.luchmun@epitech.eu>
2020-05-09 16:03:48 +0200

    first init

---
+++ app.js
@@ -0,0 +1,15 @@
+const sr = ScrollReveal({
+    duration: 1000,
+    reset: true
+});
+
+sr.reveal('h1', {
+});
+
+sr.reveal('.btn', {
+    delay: 500,
+});
+
+sr.reveal('.card', {
+    delay: 1000,
+}, 500);
